b285d67fb7132d03bf35d26715bce83e4ff52b20
azizahkurnia <mbakaziz25@gmail.com>
2018-12-27 17:37:23 +0700

    Berita Akakom Baru

---
+++ app/src/main/java/com/gmail/mbakaziz25/beritaakakombaru/DataBerita.java
@@ -0,0 +1,9 @@
+package com.gmail.mbakaziz25.beritaakakombaru;
+
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
+
